6e2f65b5d82ccd3f05ab840a3131339c1da1f954
InaValencia02 <ivalenciaamaya@udesa.edu.ar>
2021-11-13 13:28:04 -0300

    search results.js

--- JS/search-results.js
+++ JS/search-results.js
@@ -1,4 +1,10 @@
 // Resultados de Series
+
+let qs1 = location.search; // obtener la qs de la url
+let qsto1 = new URLSearchParams(qs1); // Transformar la qs en un objeto literal
+let idS = qsto1.get('id'); //Obtener el dato de id del objeto literal
+
+
 let urlSearchMovies = `https://api.themoviedb.org/3/search/tv?api_key=c7d8a8e4054747c2b47d0f7ebafc99e6&language=en-US&page=1&${busqueda}include_adult=false`;
 fetch(urlSearchMovies)
 .then(function(response){
@@ -6,6 +12,20 @@ fetch(urlSearchMovies)
 })
 .then(function(data){
     console.log(data);
+    let infoSearchMovies = data.results;
+    let seriesEncontradas = document.querySelector('.peliculasSeries');
+    let articulosSeriesEncontradas = ('');
+
+    for(let i=0; i<5; i++) {
+        articulosSeriesEncontradas += `<article>
+                                    <h3>${infoSearchMovies[i].original_title}</h3>
+                                    <div class="photo.container">
+                                    <a href="./detailMovie.html?id=${infoSearchMovies[i].id}""><img src="ttps://image.tmdb.org/t/p/original${infoSearchMovies[i].poster_path} alt=""></a>
+                                    </div>
+                                    <p>Estreno: ${infoSearchMovies[i].release_date}</p>
+                                    </article>`
+        seriesEncontradas.innerHTML = articulosSeriesEncontradas;
+    }
 })
 .catch(function(error){
     console.log(error)
