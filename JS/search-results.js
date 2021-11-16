de6f5247c3cc602972490fcd0d3380bb2d73c500
InaValencia02 <ivalenciaamaya@udesa.edu.ar>
2021-11-16 16:09:59 -0300

    progreso search results

--- JS/search-results.js
+++ JS/search-results.js
@@ -2,37 +2,41 @@
 
 let qs1 = location.search; // obtener la qs de la url
 let qsto1 = new URLSearchParams(qs1); // Transformar la qs en un objeto literal
-let idS = qsto1.get('id'); //Obtener el dato de id del objeto literal
+let queryS = qsto1.get('search'); //Obtener el dato de id del objeto literal
+
+let tituloPrincipal = document.querySelector('.tituloBusqueda');
+tituloPrincipal.innerText = `El término buscado es: ${queryS}`
 
 
-let urlSearchMovies = `https://api.themoviedb.org/3/search/tv?api_key=c7d8a8e4054747c2b47d0f7ebafc99e6&language=en-US&query=${idS}`;
-console.log(urlSearchMovies);
+let urlMovies = `https://api.themoviedb.org/3/search/tv?api_key=c7d8a8e4054747c2b47d0f7ebafc99e6&language=en-US&query=${queryS}`;
+console.log(urlMovies);
 
-fetch(urlSearchMovies)
+fetch(urlMovies)
 .then(function(response){
     return response.json()
 })
 .then(function(data){
     console.log(data);
     let infoSearchMovies = data.results;
-    let seriesEncontradas = document.querySelector('.peliculasSeries');
-    let articulosSeriesEncontradas = ('');
+    let articulosPeliculasEncontradas = ('');
+    let peliculasEncontradas = document.querySelector('.peliculasEncontradas')
 
     for(let i=0; i<5; i++) {
-        articulosSeriesEncontradas += `<article>
-                                    <h3>${infoSearchMovies[i].original_title}</h3>
+        articulosPeliculasEncontradas += `<article>
+                                    <h3>${infoSearchMovies[i].original_name}</h3>
                                     <div class="photo.container">
-                                    <a href="./detailMovie.html?id=${infoSearchMovies[i].id}""><img src="ttps://image.tmdb.org/t/p/original${infoSearchMovies[i].poster_path} alt=""></a>
+                                    <a href="./detailMovie.html?id=${infoSearchMovies[i].id}""><img src="https://image.tmdb.org/t/p/original${infoSearchMovies[i].poster_path}" class=  alt=" "></a>
                                     </div>
-                                    <p>Estreno: ${infoSearchMovies[i].release_date}</p>
+                                    <p>Estreno: ${infoSearchMovies[i].first_air_date}</p>
                                     </article>`
-        seriesEncontradas.innerHTML = articulosSeriesEncontradas;
+
+        
+        peliculasEncontradas.innerHTML = articulosPeliculasEncontradas;
     }
 })
 .catch(function(error){
     console.log(error)
 })
-
 
 // // Resultados de Series
 // let urlSearchSeries = `https://api.themoviedb.org/3/search/movie?api_key=c7d8a8e4054747c2b47d0f7ebafc99e6&language=en-US&page=1&${busqueda}include_adult=false`;
